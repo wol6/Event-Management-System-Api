0888330100eed64ae1180b2c73f001e3bf2d76d0
wol6 <alwynmathew007@gmail.com>
2026-08-10 04:21:28 +0530

    login cookie

--- controller/login/login.js
+++ controller/login/login.js
@@ -72,8 +72,9 @@ export const signIn = async (req, res) => {
         )
         res.cookie("token", token, {
             httpOnly: true,
-            // sameSite:""
-            // maxAge: 7 * 24 * 60 * 60 * 1000
+            secure: true,
+            sameSite: "none",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
         })
         return res.status(200).json({
             success: true,
