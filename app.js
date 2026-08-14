46f2f283562d8d64f35fa7748d30b3da95a4aad1
wol6 <alwynmathew007@gmail.com>
2026-08-14 12:50:36 +0530

    fx

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cookieParser())
 app.use('/api', route)
 
 app.get('/api/test',(req,res)=>{
-    res.end('Api Version : v.2.0.9')
+    res.end('Api Version : v.2.0.10')
 })
 
 app.use((err,req,res,next)=>{
